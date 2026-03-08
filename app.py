02e292f27b334cca37b920863ae92673b4236439
RZZW304 <stefan.kubik.pl@gmail.com>
2026-03-08 20:51:12 +0100

    added albums feature

--- app.py
+++ app.py
@@ -1,29 +1,36 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, abort
 import os
 
 app = Flask(__name__)
 
 PHOTO_DIR = "photos"
 
-def list_photos():
-    files = []
-    for file in os.listdir(PHOTO_DIR):
-        if file.lower().endswith((".jpg", ".jpeg", ".png", ".webp", ".gif")):
-            files.append(file)
-    files.sort(reverse=True)
-    return files
+# lista albumów
+def list_albums():
+    return [d for d in os.listdir(PHOTO_DIR) if os.path.isdir(os.path.join(PHOTO_DIR, d))]
 
+# lista zdjęć w albumie
+def list_photos(album):
+    album_path = os.path.join(PHOTO_DIR, album)
+    if not os.path.exists(album_path):
+        return []
+    return sorted([f for f in os.listdir(album_path) if f.lower().endswith((".jpg",".jpeg",".png",".gif",".webp"))])
 
 @app.route("/")
 def index():
-    photos = list_photos()
-    return render_template("index.html", photos=photos)
+    albums = list_albums()
+    return render_template("index.html", albums=albums)
 
+@app.route("/album/<album_name>")
+def album(album_name):
+    photos = list_photos(album_name)
+    if not photos:
+        abort(404)
+    return render_template("album.html", album=album_name, photos=photos)
 
-@app.route("/photos/<path:filename>")
-def photos(filename):
-    return send_from_directory(PHOTO_DIR, filename)
-
+@app.route("/photos/<album>/<filename>")
+def photos(album, filename):
+    return send_from_directory(os.path.join(PHOTO_DIR, album), filename)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
